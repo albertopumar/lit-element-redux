4f74fed0067cd177adc275348818fdf923a3ac44
Alberto <alberto.pumar.jimenez.ext@ing.com>
2020-02-21 13:26:53 +0100

    add test structure

---
+++ tests/connect.test.js
@@ -0,0 +1,44 @@
+import { combineReducers } from 'redux'
+import connect from '../src/connect';
+import createStore from '../src/createStore';
+import counterReducer, { operations } from './mock';
+
+describe("createStore test", () => {
+
+  let mockConnectedClass;
+
+  beforeAll(() => {
+    // Initialize the store before running all the tests
+    const rootReducer = combineReducers({ counter: counterReducer });
+    createStore(rootReducer);
+
+    // Connect a javascript class to the store
+    const mapStateToProps = store => ({
+      counter: store.counter,
+    });
+    const mapDispatchToProps = { ...operations };
+
+    class MyTestClass { }
+
+    const ConnectedClass = connect(mapStateToProps, mapDispatchToProps)(MyTestClass);
+    mockConnectedClass = new ConnectedClass();
+  });
+
+  it('connect give you access to the store', () => {
+    expect(mockConnectedClass.counter).toBeDefined();
+  });
+
+  it('connect give you access to dispatch functions', () => {
+    expect(mockConnectedClass.increment).toBeDefined();
+  });
+
+  it('mapDispatchToProps can be an object', () => {
+
+    expect(true).toEqual(true);
+  });
+
+  it('mapDispatchToProps can be a function with dispatch', () => {
+
+    expect(true).toEqual(true);
+  });
+});
---
+++ tests/createStore.test.js
@@ -0,0 +1,26 @@
+import { combineReducers } from 'redux'
+import createStore, { getStore } from '../src/createStore';
+import counterReducer from './mock';
+
+describe("createStore test", () => {
+  beforeAll(() => {
+    // Initialize the store before running all the tests
+    const rootReducer = combineReducers({ counter: counterReducer });
+
+    createStore(rootReducer);
+  });
+
+  it('expose the store after created with "createStore"', () => {
+    // Retrieve the store and check if it was initialized
+    const store = getStore();
+    expect(store).toBeDefined();
+  });
+
+  it('initial store structure is defined', () => {
+    // Retrieve the store and check if it has proper structure
+    const store = getStore();
+
+    const expectedStore = { counter: 0 }
+    expect(store.getState()).toMatchObject(expectedStore);
+  });
+});
